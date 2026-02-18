a153f479b02c76b2e0a80e079ac6dca7fcaae819
Sena Oz <tazeyta@gmail.com>
2026-02-18 21:15:56 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = OpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-3-flash-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the capital city of Turkiye?"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
+
+
+
+
+
